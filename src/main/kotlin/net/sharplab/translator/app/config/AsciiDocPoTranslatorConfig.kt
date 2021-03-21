7d75bcdfd42a8c53c9973fb49f347b47f2057cef
ynojima <mail@ynojima.net>
2021-03-21 17:03:33 +0900

    Bugfix: config handling

--- src/main/kotlin/net/sharplab/translator/app/config/AsciiDocPoTranslatorConfig.kt
+++ src/main/kotlin/net/sharplab/translator/app/config/AsciiDocPoTranslatorConfig.kt
@@ -14,23 +14,23 @@ class AsciiDocPoTranslatorConfig(private val asciiDocPoTranslatorSetting: AsciiD
     @Produces
     fun translator(): Translator
     {
-        return when (asciiDocPoTranslatorSetting.engine?.toLowerCase()){
+        return when (asciiDocPoTranslatorSetting.engine.toLowerCase()){
             "globalese".toLowerCase() -> {
                 val globaleseEndpoint = asciiDocPoTranslatorSetting.globaleseEndpoint
                 val globaleseUsername = asciiDocPoTranslatorSetting.globaleseUsername
                 val globalesePassword = asciiDocPoTranslatorSetting.globaleseApiKey
                 val globaleseProjectId = asciiDocPoTranslatorSetting.globaleseProjectId
-                GlobaleseTranslator(globaleseEndpoint!!, globaleseUsername!!, globalesePassword!!, globaleseProjectId!!)
+                GlobaleseTranslator(globaleseEndpoint.get(), globaleseUsername.get(), globalesePassword.get(), globaleseProjectId.get())
             }
             "deepL".toLowerCase() -> {
                 val deepLApiKey = asciiDocPoTranslatorSetting.deepLApiKey
-                DeepLTranslator(deepLApiKey!!)
+                DeepLTranslator(deepLApiKey.get())
             }
             "msTranslator".toLowerCase() -> {
                 val msTranslatorSubscriptionKey = asciiDocPoTranslatorSetting.msTranslatorSubscriptionKey
                 val msTranslatorLocation = asciiDocPoTranslatorSetting.msTranslatorLocation
                 val msTranslatorCategory = asciiDocPoTranslatorSetting.msTranslatorCategory
-                MSTranslator(msTranslatorSubscriptionKey!!, msTranslatorLocation!!, msTranslatorCategory!!)
+                MSTranslator(msTranslatorSubscriptionKey.get(), msTranslatorLocation.get(), msTranslatorCategory.get())
             }
             else -> throw RuntimeException("Configuration 'translator.engine' must be 'deepL' or 'msTranslator'.")
         }
--- src/main/kotlin/net/sharplab/translator/app/setting/AsciiDocPoTranslatorSetting.kt
+++ src/main/kotlin/net/sharplab/translator/app/setting/AsciiDocPoTranslatorSetting.kt
@@ -1,28 +1,29 @@
 package net.sharplab.translator.app.setting
 
 import org.eclipse.microprofile.config.inject.ConfigProperty
+import java.util.*
 import javax.enterprise.context.Dependent
 
 @Dependent
 class AsciiDocPoTranslatorSetting {
     @ConfigProperty(name = "translator.engine")
-    var engine: String? = null
+    var engine: String = "deepL"
     @ConfigProperty(name = "translator.globalese.endpoint")
-    var globaleseEndpoint: String? = null
+    var globaleseEndpoint: Optional<String> = Optional.empty()
     @ConfigProperty(name = "translator.globalese.username")
-    var globaleseUsername: String? = null
+    var globaleseUsername: Optional<String> = Optional.empty()
     @ConfigProperty(name = "translator.globalese.apiKey")
-    var globaleseApiKey: String? = null
+    var globaleseApiKey: Optional<String> = Optional.empty()
     @ConfigProperty(name = "translator.globalese.projectId")
-    var globaleseProjectId: Int? = null
+    var globaleseProjectId: Optional<Int> = Optional.empty()
     @ConfigProperty(name = "translator.deepL.apiKey")
-    var deepLApiKey: String? = null
+    var deepLApiKey: Optional<String> = Optional.empty()
     @ConfigProperty(name = "translator.msTranslator.subscriptionKey")
-    var msTranslatorSubscriptionKey: String? = null
+    var msTranslatorSubscriptionKey: Optional<String> = Optional.empty()
     @ConfigProperty(name = "translator.msTranslator.location")
-    var msTranslatorLocation: String? = null
+    var msTranslatorLocation: Optional<String> = Optional.empty()
     @ConfigProperty(name = "translator.msTranslator.category")
-    var msTranslatorCategory: String? = null
+    var msTranslatorCategory: Optional<String> = Optional.empty()
     @ConfigProperty(name = "translator.language.source", defaultValue = "en")
     var defaultSrcLang: String? = null
     @ConfigProperty(name = "translator.language.destination", defaultValue = "ja")
